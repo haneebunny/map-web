afa1af9db017632365c881eed753c040f6a1fb12
PADUCK <re____@naver.com>
2023-05-12 12:31:18 +0900

    deploy to gh-pages

---
+++ out/_next/static/9X5d2aZoD8fDMz_0Q98pC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-ee3534f8d2b90090.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
