50b17b8ac1654a2a23f5c9b4ba90865cc076b7dd
PADUCK <re____@naver.com>
2023-06-07 18:33:48 +0900

    :sparkles: My server
    bbu deut

---
+++ pages/api/parking-lot.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import axios from "axios";
+
+const PARKING_LIST_URL = `http://211.252.37.224/rest/parking`;
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      let { data } = await axios.get(
+        `${PARKING_LIST_URL}/?keyword=광주&parkingFee=무료&page=1&pageSize=2`
+      );
+
+      console.log("dd", data);
+      return res.status(200).send(data.parkingInfoList);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).send(e.message);
+    }
+  }
+
+  return res.status(405).end();
+}
--- src/common/hook/useMap.jsx
+++ src/common/hook/useMap.jsx
@@ -1,97 +1,75 @@
 import { useCallback } from "react";
 import { useState } from "react";
 
-export default function useMap() {
+export default function useMap(mapContainer, setMarkerImage, markerImage, DB) {
   const [map, setMap] = useState(null);
   const [markers, setMarkers] = useState([]);
 
   const makeMap = useCallback(() => {
-    const script = document.createElement("script");
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=9d0f0e60b8c9641a75a00620cda24d1b&autoload=false&libraries=services`;
-    document.head.appendChild(script);
+    const options = {
+      center: new window.kakao.maps.LatLng(37.4932385, 126.9175228),
+      level: window.innerHeight < 1300 ? 4 : 3, // 지도 확대 레벨
+    };
+    // 지도를 표시할 div와 지도 옵션으로 지도를 생성함
+    const newMap = new window.kakao.maps.Map(mapContainer.current, options);
 
-    window.kakao.maps.load(function () {
-      const container = document.getElementById("map");
-      const options = {
-        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3, // 지도 확대 레벨
-      };
+    // 지도 확대 축소를 제어할 수 있는 줌 컨트롤 생성
+    const zoomControl = new window.kakao.maps.ZoomControl();
+    newMap.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
 
-      const map = new window.kakao.maps.Map(container, options);
-      // 지도 생성
+    // *마커이미지*
+    const imageSrc = "img/marker.png"; // 마커이미지의 주소
+    const imageSize = new window.kakao.maps.Size(64, 69); // 마커이미지의 크기
+    const imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
-      const mapTypeControl = new kakao.maps.MapTypeControl();
-      // 지도 컨트롤 생성
+    const markerImage = new window.kakao.maps.MarkerImage(
+      imageSrc,
+      imageSize,
+      imageOption
+    );
 
-      map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-      // 지도 오른쪽 위에 지도 컨트롤
+    setMarkerImage(new kakao.maps.MarkerImage(imageSrc, imageSize));
 
-      const zoomControl = new kakao.maps.ZoomControl();
-      map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-      // 확대, 축소 제어 줌  컨트롤 생성
+    setMap(newMap);
 
-      // *마커이미지*
-      const imageSrc = "img/marker.png"; // 마커이미지의 주소
-      const imageSize = new window.kakao.maps.Size(64, 69); // 마커이미지의 크기
-      const imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    // makeMap
+  }, [mapContainer, setMarkerImage]);
 
-      const markerImage = new window.kakao.maps.MarkerImage(
-        imageSrc,
-        imageSize,
-        imageOption
+  // 마커를 생성하는 함수
+  const makeMarkers = useCallback(() => {
+    if (!markerImage) return;
+
+    // 기존 마커 제거
+    if (markers.length > 0) {
+      markers.forEach((marker) => marker.setMap(null));
+    }
+
+    // 마커 표시하기
+
+    const newMarkers = [];
+    DB.forEach((store) => {
+      const marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        title: store.FCLTY_NM, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨
+        position: new kakao.maps.LatLng(store.FCLTY_LA, store.FCLTY_LO), // 마커를 표시할 위치(위도, 경도)
+        image: markerImage, // 커스텀 마커 이미지 설정
+        id: store.ESNTL_ID, // 마커에 ESNTL_ID를 id로 설정
+      });
+
+      // 마커 클릭시 해당 bookstoreId로 라우터 이동
+      kakao.maps.event.addListener(marker, "click", () =>
+        navigate(`/map/${store.ESNTL_ID}`)
       );
 
-      // 지도 주소로 검색
-      const geocoder = new window.kakao.maps.services.Geocoder();
-
-      // 현재 위치 마커
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-          const lat = position.coords.latitude; // 위도
-          const lng = position.coords.longitude; // 경도
-
-          const markerPosition = new kakao.maps.LatLng(lat, lng);
-
-          // 마커를 생성하고 지도에 표시합니다.
-          const marker = new kakao.maps.Marker({
-            position: markerPosition,
-          });
-          marker.setMap(map);
-
-          // 지도의 중심을 현재 위치로 이동시킵니다.
-          map.setCenter(markerPosition);
-        });
-      }
-
-      // 주소로 검색한 마커
-      POSITIONS.map((position) => {
-        geocoder.addressSearch(position, function (result, status) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            const coords = new window.kakao.maps.LatLng(
-              result[0].y,
-              result[0].x
-            );
-
-            const marker = new window.kakao.maps.Marker({
-              map,
-              position: coords,
-              image: markerImage,
-            });
-
-            // 마커 클릭
-            window.kakao.maps.event.addListener(marker, "click", function () {
-              console.log("1");
-              setIsBottomSheetVisible(!isBottomSheetVisible);
-              console.log("is", isBottomSheetVisible);
-            });
-
-            // 지도의 초점
-            map.setCenter(coords);
-          }
-        });
-      });
+      // 마커를 배열에 저장
+      newMarkers.push(marker);
     });
 
-    //
-  });
+    // 마커 배열을 state에 저장
+    setMarkers(newMarkers);
+
+    // eslint-disable-next-line
+  }, [DB, map, markerImage]);
+
+  return { makeMap, makeMarkers, map };
 }
