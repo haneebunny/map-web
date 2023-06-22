c488b0ffe7bacbb86f5a8d6800091357f6a0cf2f
PADUCK <re____@naver.com>
2023-06-22 16:42:32 +0900

    🧹 안 쓰는 파일 정리

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,13 @@
-import { RecoilRoot, useRecoilState } from "recoil";
+import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { useEffect, useState } from "react";
-import io from "socket.io-client";
-
 const queryClient = new QueryClient();
-
 import "../styles/globals.css";
-import {
-    handleNoticeMessage,
-    notifyGiveNotice,
-    onChatMessage,
-    onConnect,
-    onDisconnect,
-} from "../src/common/api/socket";
-import { Events } from "../src/components/socket/Events";
-import useSocket from "../src/common/hook/useSocket";
-import App from "next/app";
-import { noticeMessage } from "../src/common/store/atom";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
             <QueryClientProvider client={queryClient}>
                 <RecoilRoot>
-                    <Events />
                     <Component {...pageProps} />
                 </RecoilRoot>
             </QueryClientProvider>
